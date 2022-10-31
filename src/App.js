a7df8dc0a2ce3bd398e5fd03a21d25e276f7109c
Kazi Rahat <kazirahat1007@outlook.com>
2022-10-31 11:02:42 +0600

    create header and footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/Routes/Routes";
+
 function App() {
    return (
-      <div>
-         <h2>Hello Genius Car</h2>
+      <div className="max-w-[1240px] mx-auto">
+         <RouterProvider router={router} />
       </div>
    );
 }
---
+++ src/Pages/index.js
@@ -0,0 +1,4 @@
+import Home from "./Home/Home/Home";
+import Header from "./Shared/Header/Header";
+import Footer from "./Shared/Footer/Footer";
+export { Home, Header, Footer };
